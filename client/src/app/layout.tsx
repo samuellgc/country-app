08b4c82c79d39ef38e04babcb37bb3217eacccff
samuellgc <samuellgc@hotmail.com>
2024-11-21 11:16:29 -0300

    [FEAT]: Initial configuration

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./assets/css/globals.css";
+
+const geistSans = localFont({
+  src: "./assets/fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./assets/fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Country App",
+  description: "Discover more about the world's countries!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
